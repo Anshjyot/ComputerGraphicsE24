36ac38bca3bcaed5644e17892cc44d952bcbcc8e
Anshjyot <s215806@dtu.dk>
2024-10-09 20:18:53 +0200

    Changes in structure

---
+++ ComputerGraphicsE24/Week1/Part1/worksheet1_part1.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var canvas = document.getElementById("glCanvas");
+    var gl = setupWebGL(canvas);  
+
+    if (!gl) {
+        console.error("WebGL not supported");
+        return;
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0); 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+};
